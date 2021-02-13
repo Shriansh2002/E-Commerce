3438e9604dcd390ef3a918026d85a65aff3fed2c
SHRIANSH Coding Tutorials <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2021-02-13 13:50:19 +0530

    Updated Settings.py for Security Purpose

--- ShrianshWEB/settings.py
+++ ShrianshWEB/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=xo83mr1#&pmwcreu@x)t)k_iy3@7hvte!wg2do4$&*ov&79@h'
+SECRET_KEY = 'keep your secret key here!!!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
